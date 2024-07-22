645fcd3a28c3b281bcc564240274d909b9b25aed
Joshua Onungwe <onungwej@gmail.com>
2024-07-21 20:35:45 -0400

    implemented stripe payments

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,10 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 export default clerkMiddleware({});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.*\\..*|_next).*)',
+    '/',
+    '/(api|trpc)(.*)',
+    '/api/webhooks/stripe',
+  ],
 };
